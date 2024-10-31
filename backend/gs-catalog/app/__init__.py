dee7935e1cbc545c8b5b93185b08e449d75e936a
Kay Han <kyungwanhan@gmail.com>
2024-10-31 11:22:02 -0400

    feat(backend): Add scripts for docker build

--- backend/gs-catalog/app/__init__.py
+++ backend/gs-catalog/app/__init__.py
@@ -1,17 +1,11 @@
 from flask import Flask
 from flask_cors import CORS
+from .api.routes import configure_routes
 
-from .api.health import health_blueprint
-from .api.routes import api_blueprint
 
-def configure_routes(app):
-    app.register_blueprint(health_blueprint, url_prefix='/health')
-    app.register_blueprint(api_blueprint, url_prefix='/api')
-    
 def create_app():
-  app = Flask(__name__)
-  CORS(app)
-  
-  configure_routes(app)
+    app = Flask(__name__)
+    CORS(app)
+    configure_routes(app)
 
-  return app
+    return app
